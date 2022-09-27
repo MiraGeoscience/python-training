2bfb5766f485e8f96d100d58688ca1f3ef98f1fd
dominiquef <dominiquef@mirageoscience.com>
2022-09-26 17:55:23 -0700

    Run linter on notebook scripts

--- tests/notebook_test.py
+++ tests/notebook_test.py
@@ -3,29 +3,30 @@
 #  This file is part of python-training.
 
 import os
+import shutil
 import subprocess
 
-from nbconvert import ScriptExporter, export
 
-
-def test_notebooks():
+def test_notebooks(tmp_path):
     os.makedirs("notebooks", exist_ok=True)
     failures = []
 
-    for directory, sub_dir, files in os.walk(os.path.join("..", "training")):
+    for directory, _, files in os.walk(os.path.join("..", "training")):
         if ".ipynb_checkpoints" in directory or "_build" in directory:
             continue
+
         for file in files:
-            if file.endswith(".ipynb"):
-                file_name = os.path.join("notebooks", file[:-6] + ".py")
-                with open(file_name, mode="w") as script:
-                    content = export(ScriptExporter, os.path.join(directory, file))[0]
-                    script.write(content)
 
-                process = subprocess.run(["python", file_name], capture_output=True)
+            if not file.endswith(".py"):
+                continue
 
-                if not process.returncode == 0:
-                    failures.append(process)
+            shutil.copy(os.path.join(directory, file), tmp_path / file)
+            process = subprocess.run(
+                ["python", tmp_path / file], capture_output=True, check=True
+            )
+
+            if not process.returncode == 0:
+                failures.append(process)
 
         if any(failures):
             raise RuntimeError(failures)
---
+++ training/best_practice.py
@@ -0,0 +1,36 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.14.0
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# # Best Practice
+#
+# This section provides a brief overview of best coding practice. Knowing that coding standards may change over time,
+# the information provided below reflects principals established by the community and documented in the [PEP8 style
+# guide](https://peps.python.org/pep-0008/).
+
+# ## Docstrings
+#
+# One important part of open-source work is to provide sufficient information on classes and methods for others to
+# use. The main source of information is provided as a block of text, also referred to as docstring, directly below
+# the signature of the method. The information should be written in plain English with a list of input arguments.
+# Different styles are possible and generally agreed upon by the development team.
+#
+# - [sphinx-reST](https://sphinx-rtd-tutorial.readthedocs.io/en/latest/docstrings.html)
+# - [numpydoc](https://numpydoc.readthedocs.io/en/latest/format.html)
+# - [google](https://github.com/google/styleguide/blob/gh-pages/pyguide.md#38-comments-and-docstrings)
+#
+# Regardless of the style chosen, it is all about consistency.
+#
+# In this tutorial, we follow the `sphinx-reST` style as it is compact and standard format with [Pycharm](
+# https://www.jetbrains.com/pycharm) used for code development.
