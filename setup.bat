515165a73bbea86f84cf79cb84517450e0bdaad7
dominiquef <dominiquef@mirageoscience.com>
2022-11-19 08:35:08 -0800

    Move pyfiles back to content

--- setup.bat
+++ setup.bat
@@ -11,6 +11,6 @@ cd %~dp0
 set PYTHONUTF8=1
 call "!MY_CONDA!" remove --name python-training --all --yes
 call "!MY_CONDA!" env update -f environments\conda-py-3.9-win-64.lock.yml -n python-training
-call "!MY_CONDA!" activate python-training && python -m pip install -e . --no-deps
+call "!MY_CONDA!" activate python-training
 pause
 cmd /k
