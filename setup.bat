63ad1a2d6b3b00e6619683750fa9dc87018defd0
dominiquef <dominiquef@mirageoscience.com>
2023-01-19 17:38:35 -0800

    Update installer to fix SSL error

--- setup.bat
+++ setup.bat
@@ -3,14 +3,20 @@ setlocal EnableDelayedExpansion
 
 call "%~dp0get_conda_exec.bat"
 if !errorlevel! neq 0 (
+  pause
   exit /B !errorlevel!
 )
+
+set PY_VER=3.10
 
 set MY_CONDA=!MY_CONDA_EXE:"=!
 cd %~dp0
 set PYTHONUTF8=1
-call "!MY_CONDA!" remove --name python-training --all --yes
-call "!MY_CONDA!" env update -f environments\conda-py-3.9-win-64.lock.yml -n python-training
-call "!MY_CONDA!" activate python-training
+call "!MY_CONDA!" activate
+call conda remove --name python-training --all --yes
+call conda env create -f environments\conda-py-%PY_VER%-win-64.lock.yml -n python-training
+call conda activate python-training
+pip install -e . --no-deps
+
 pause
 cmd /k
