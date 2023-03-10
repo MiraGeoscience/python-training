b6243c597c111fca0adcb4f0639cc95009fbde02
sebhmg <sebastienh@mirageoscience.com>
2023-03-10 11:28:37 -0500

    more robust setup and start scripts

--- start_training.bat
+++ start_training.bat
@@ -3,9 +3,23 @@ setlocal EnableDelayedExpansion
 
 call "%~dp0get_conda_exec.bat"
 if !errorlevel! neq 0 (
+  pause
   exit /B !errorlevel!
 )
 
+set ENV_NAME=python-training
+
 set MY_CONDA=!MY_CONDA_EXE:"=!
-call "!MY_CONDA!" activate python-training && jupyter notebook training\index.ipynb
-cmd /k
+cd %~dp0
+set PYTHONUTF8=1
+
+call "!MY_CONDA!" run -n %ENV_NAME% jupyter notebook training\index.ipynb
+
+if !errorlevel! neq 0 (
+  echo "** ERROR: Failed starting the notebook **"
+  pause
+  exit /B !errorlevel!
+)
+
+pause
+cmd /k "!MY_CONDA! activate %ENV_NAME%"
